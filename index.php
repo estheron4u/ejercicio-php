4542d4daebf732b89255bf47c535d70bd5b78c6a
estheron4u <esther@on4u.es>
2022-04-25 10:55:47 +0200

    Add project files

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Get login data
+$xml=simplexml_load_file("login.xml") or die("Error: Cannot create object");
+
+// Declare variables
+$servername = "localhost";
+$username = $xml->user;
+$password = $xml->password;
+$database = "classicmodels";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Select data
+$sql = "SELECT customerName FROM customers";
+$result = $conn->query($sql);
+
+// Print data
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Customer: " . $row["customerName"] . "\n";
+    }
+} else {
+    echo "0 results";
+}
