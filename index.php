d8f7d1d4350497fd018d9a0c40af44a3ed0cd103
estheron4u <esther@on4u.es>
2022-05-03 07:55:35 +0200

    add xml validation and transform procedural code into oop

--- index.php
+++ index.php
@@ -1,31 +1,58 @@
 <?php
 
 // Get login data
-$xml=simplexml_load_file("login.xml") or die("Error: Cannot create object");
+$xml=simplexml_load_file("login.xml");
 
-// Declare variables
-$servername = "localhost";
-$username = $xml->user;
-$password = $xml->password;
-$database = "classicmodels";
+//Check login data
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+if (!preg_match("/[a-zA-Z\d_.-]+/", $xml->user) or !preg_match("/[a-zA-Z_.-]+/", $xml->password)){
+    die("Error: Cannot create object");
+};
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+class DatabaseConnection {
 
-// Select data
-$sql = "SELECT customerName FROM customers";
-$result = $conn->query($sql);
+    // Declare variables
+    private $servername;
+    private $username;
+    private $password;
+    private $database;
 
-// Print data
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "Customer: " . $row["customerName"] . "\n";
+    function __construct($servername, $username, $password, $database) {
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
     }
-} else {
-    echo "0 results";
+
+    // Create connection
+    protected function connect() {
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
+    }
 }
+
+class CustomerNames extends DatabaseConnection {
+
+    // Select data
+    public function getCustomers() {
+        $sql = "SELECT customerName FROM customers";
+        $result = $this->connect()->query($sql);
+
+        // Print data
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "Customer: " . $row["customerName"] . "\n";
+            }
+        } else {
+            echo "0 results";
+        }
+    }
+}
+
+$customers = new CustomerNames("localhost", $xml->user, $xml->password, "classicmodels");
+echo $customers->getCustomers();
