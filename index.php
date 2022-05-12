5447dd543c13730c9bcf36f22be763ace71f18b4
estheron4u <esther@on4u.es>
2022-05-12 12:43:26 +0200

    add GetLoginData class to get xml data

--- index.php
+++ index.php
@@ -2,40 +2,56 @@
 
 include('config.php');
 
-// Get login data
-$xml=simplexml_load_file("login.xml"); //TODO why is this outside of class
-//TODO also you removed a validation
+class GetLoginData {
+    public $xml;
 
-//Check login data //TODO this comment could be a function name
+    public function __construct(){
+        $this->xml = simplexml_load_file("login.xml") or die("Error: Cannot create object");
+    }
 
-if (!preg_match("/[a-zA-Z\d_.-]+/", $xml->user) or !preg_match("/[a-zA-Z_.-]+/", $xml->password)){ //TODO are you checking it exists, or the format
-    die("Error: Cannot create object"); //TODO what object?
+    protected function checkLoginData(){
+        if (!$this->xml->user){
+            die("Error: User field doesn't exist");
+        }
+        if (!$this->xml->password){
+            die("Error: Password field doesn't exist");
+        }
+        if (!preg_match("/[a-zA-Z\d_.-]+/", $this->xml->user)){
+            die("Error: User is not correct");
+        }
+        if (!preg_match("/[a-zA-Z\d_.-]+/", $this->xml->password)) {
+            die("Error: Password is not correct");
+        }
+    }
 };
 
 class DatabaseConnection { //TODO Why not use file for each class
 
-    // Declare variables //TODO not really relevant
+    private $logindata;
+
     private $servername;
     private $username;
     private $password;
     private $database;
 
-    function __construct($servername, $username, $password, $database) { //TODO access modifier missing
+    public function __construct($servername, $database) {
+        $this->logindata = new GetLoginData;
+
         $this->servername = $servername;
-        $this->username = $username;
-        $this->password = $password;
+        $this->username = $this->logindata->xml->user;
+        $this->password = $this->logindata->xml->password;
         $this->database = $database;
     }
 
     // Create connection
     protected function connect() {
-        $conn = new mysqli($this->servername, $this->username, $this->password, $this->database); //TODO no need to abbreviate 'connection' to 'conn'
+        $connection = new mysqli($this->servername, $this->username, $this->password, $this->database);
 
         // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error); //TODO die could be dangerous, exceptions are safer
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error); //TODO die could be dangerous, exceptions are safer
         }
-        return $conn;
+        return $connection;
     }
 }
 
@@ -57,7 +73,6 @@ class CustomerNames extends DatabaseConnection { //TODO why extend?
     }
 }
 
-$customers = new CustomerNames($host, $xml->user, $xml->password, $database);
+$customers = new CustomerNames($host, $database);
 echo $customers->getCustomers();
 
-//TODO one git commit for 2 changes make sit it difficult to analyse history
