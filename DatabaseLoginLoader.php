f3b38404e923e961cbe63619054f52b0d8823cd9
estheron4u <esther@on4u.es>
2022-07-12 08:07:08 +0200

    refactor: check login data inside getters

--- DatabaseLoginLoader.php
+++ DatabaseLoginLoader.php
@@ -8,63 +8,63 @@ class DatabaseLoginLoader {
     private $database;
 
     public function __construct(){
-        $this->xml = $this->checkDatabaseLogin(); //TODO probably too slow for a construct - Esto es que se puede hacer el check en los getters en vez de en el construct
+        $this->xml = simplexml_load_file("login.xml");
         $this->server = $this->xml->server;
         $this->username = $this->xml->user;
         $this->password = $this->xml->password;
         $this->database = $this->xml->database;
     }
 
-    private function checkDatabaseLogin(){
-        $xml = simplexml_load_file("login.xml");//TODO const candidate
-        $msg = '';
-        if(!$xml){
-            $msg .= "Cannot load login data file\n";
-        }
-        if (!$xml->user){
-            $msg .= "User field doesn't exist in login data file\n";
-        }
-        if (!$xml->password){
-            $msg .= "Password field doesn't exist in login data file\n";
-        }
-        if (!preg_match("/^[a-zA-Z\d]\w{1,14}$/", $xml->user)){
-            $msg .= "User format is not correct in login data file\n";
-        }
-        if (!preg_match("/^[a-zA-Z\d]\w{1,14}$/", $xml->password)) {
-            $msg .= "Password format is not correct in login data file\n";
-        }
-        if (strlen($msg) > 0){
-            throw new Exception($msg);
-        }
-        return $xml;
-    }
-
-
     /**
      * @return string
+     * @throws Exception
      */
     public function getServer() : string {
+        if(!$this->xml){
+            throw new Exception("Cannot load login data file");
+        }
         return $this->server->__toString();
     }
 
     /**
      * @return string
+     * @throws Exception
      */
     public function getUsername() : string{
+        if(!$this->xml){
+            throw new Exception("Cannot load login data file");
+        }
+        if (!$this->xml->user){
+            throw new Exception("User field doesn't exist in login data file");
+        }
+        if (!preg_match("/^[a-zA-Z\d]\w{1,14}$/", $this->xml->user)){
+            throw new Exception("User format is not correct in login data file");
+        }
         return $this->username->__toString();
     }
 
     /**
      * @return string
+     * @throws Exception
      */
     public function getPassword() : string{
+        if (!$this->xml->password){
+            throw new Exception("Password field doesn't exist in login data file");
+        }
+        if (!preg_match("/^[a-zA-Z\d]\w{1,14}$/", $this->xml->password)) {
+            throw new Exception("Password format is not correct in login data file");
+        }
         return $this->password->__toString();
     }
 
     /**
      * @return string
+     * @throws Exception
      */
     public function getDatabase() : string{
+        if(!$this->xml){
+            throw new Exception("Cannot load login data file");
+        }
         return $this->database->__toString();
     }
 
