35929aaddcd4067c2b09fe341e5443b31021f97f
estheron4u <esther@on4u.es>
2022-07-21 13:57:56 +0200

    ejercicio_2 refactor: split connection validation into two ifs

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -1,21 +1,24 @@
 <?php
+
 include_once('DatabaseLoginLoader.php');
 
-class DatabaseConnector {
+class DatabaseConnector
+{
 
     private $connection;
 
-    public function connectToDatabase(): mysqli {
-
+    public function connectToDatabase(): mysqli
+    {
         $logindata = new DatabaseLoginLoader();
         $server = $logindata->getServer();
         $username = $logindata->getUsername();
         $password = $logindata->getPassword();
         $database = $logindata->getDatabase();
 
-        if ($this->connection == null) { //TODO strict comparison , when viable, makes for a more robust code
+        if ($this->connection === null) {
             $this->connection = new mysqli($server, $username, $password, $database);
-        } else if ($this->connection->connect_error) { //TODO if new mysqli gives error, no exception is thrown. I'll probably split this else into a doble if
+        }
+        if ($this->connection->connect_error) {
             throw new Exception("Connection failed: " . $this->connection->connect_error);
         }
         return $this->connection;
