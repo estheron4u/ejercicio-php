59699763b9262cbaf31e7ebc4fcf8c74a231a0f3
estheron4u <esther@on4u.es>
2022-08-03 15:24:54 +0200

    ejercicio_3 format: delete empty lines

--- DatabaseDataPrinter.php
+++ DatabaseDataPrinter.php
@@ -9,13 +9,11 @@ class DatabaseDataPrinter
      */
     public function printCustomerNames($data)
     {
-        $customerNames = $data; //TODO no need for so many empty separator lines. Empty lines are considered a code smell
-
+        $customerNames = $data;
         if (count($customerNames) <= 0) {
             echo "0 results";
             return;
         }
-
         foreach ($customerNames as $customer) {
             echo "Customer: {$customer['customerName']}\n"; //TODO if 'customerName' does not exist, it will fail. You should check its existence before accessing it. You could also declare the expected structure in the PHPDoc
         }
