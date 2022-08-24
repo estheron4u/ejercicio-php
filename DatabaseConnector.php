03a95022fa4dea0740c407f6aa0a38a4cbcf6783
estheron4u <esther@on4u.es>
2022-08-24 13:55:20 +0200

    ejercicio_3 fix: delete accidental comments

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -4,7 +4,6 @@ include_once('DatabaseLoginLoader.php');
 
 class DatabaseConnector
 {
-//    private const CUSTOMER_NAMES_QUERY = "SELECT customerName FROM customers"; //TODO - BAD PRACTICE: yuo have git to recover past code, no need to preserve it commented
     private $server;
     private $username;
     private $password;
@@ -33,37 +32,4 @@ class DatabaseConnector
         }
         return $this->connection;
     }
-    //TODO - BAD PRACTICE: yuo have git to recover past code, no need to preserve it commented
-//
-//    /**
-//     * @throws Exception
-//     */
-//    public function getCustomerNames(): array
-//    {
-//        $connection = $this->getDatabaseConnection();
-//        $result = $connection->query(self::CUSTOMER_NAMES_QUERY);
-//        $customerNames = [];
-//        while ($row = $result->fetch_assoc()) {
-//            $customerNames[] = $row;
-//        }
-//        return $customerNames;
-//    }
-
-//    /**
-//     * @throws Exception
-//     */
-//    public function getCustomerNamesByCity($city): array
-//    {
-//        $query = self::CUSTOMER_NAMES_QUERY . " WHERE city = ?";
-//        $connection = $this->getDatabaseConnection();
-//        $preparedStatement = $connection->prepare($query);
-//        $preparedStatement->bind_param('s', $city);
-//        $preparedStatement->execute();
-//        $result = $preparedStatement->get_result();
-//        $customerNames = [];
-//        while ($row = $result->fetch_assoc()) {
-//            $customerNames[] = $row;
-//        }
-//        return $customerNames;
-//    }
 }
