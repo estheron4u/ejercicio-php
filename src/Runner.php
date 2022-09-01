b29d11e379a9acc69323579d1400ee65c174eae2
estheron4u <esther@on4u.es>
2022-09-01 10:06:51 +0200

    ejercicio_4 refactor: use switch instead of ifelse and allow to try agai if the inserted value is incorrect

--- src/Runner.php
+++ src/Runner.php
@@ -50,7 +50,7 @@ class Runner
         $city = $input->readTerminal('Insert name of the city you want to view customers from: ');
 
         $dataLoader = new DataLoaderFactory();
-        $dataLoader = $dataLoader->getLoaderService( $dataLoader::CustomersByCityDatabase);
+        $dataLoader = $dataLoader->getLoaderService($dataLoader::CustomersByCityDatabase);
         $dataLoader->setConnection($connection);
         $dataLoader->setCity($city);
         return $dataLoader->getCustomerNames();
@@ -103,26 +103,40 @@ class Runner
     {
         $input = new TerminalReader();
 
-        $databaseType = strtolower($input->readTerminal('Type desired database type (MySQL or CSV): '));
-        if ($databaseType === 'mysql') { // FIXME magic string
-            $connectorType = strtolower($input->readTerminal('Type desired connector type (json or xml): '));
-            $connector = $this->getConnector($connectorType);
-            $connection = $connector->getDatabaseConnection();
+        $validDatabaseType = false;
+        while ($validDatabaseType === false) {
+            $databaseType = strtolower($input->readTerminal('Type desired database type (MySQL or CSV): '));
+            switch ($databaseType) {
+                case 'mysql':
+                    $connectorType = strtolower($input->readTerminal('Type desired connector type (json or xml): '));
+                    $connector = $this->getConnector($connectorType);
+                    $connection = $connector->getDatabaseConnection();
 
-            $filtering = strtolower($input->readTerminal('Filter customers by city? (Yes/No): '));
+                    $validFilteringType = false;
+                    while ($validFilteringType === false) {
+                        $filtering = strtolower($input->readTerminal('Filter customers by city? (Yes/No): '));
+                        if ($filtering === 'yes') {
+                            $customerNames = $this->getDatabaseCustomersByCity($connection);
+                            $validFilteringType = true;
+                        } elseif ($filtering === 'no') {
+                            $customerNames = $this->getDatabaseCustomers($connection);
+                            $validFilteringType = true;
+                        } else {
+                            echo "Invalid answer, please try again\n";
+                        }
+                    }
 
-            if ($filtering === 'yes') {
-                $customerNames = $this->getDatabaseCustomersByCity($connection);
-            } else {  // TODO so... 'potato' also means 'no'?
-                $customerNames = $this->getDatabaseCustomers($connection);
+                    $customers = new DatabaseDataPrinter();
+                    $customers->printCustomerNames($customerNames);
+                    $validDatabaseType = true;
+                    break;
+                case 'csv':
+                    $this->runCustomersCsv();
+                    $validDatabaseType = true;
+                    break;
+                default:
+                    echo "Invalid database type, please try again\n";
             }
-
-            $customers = new DatabaseDataPrinter();
-            $customers->printCustomerNames($customerNames);
-        } elseif ($databaseType === 'csv'){ //FIXME PSR!
-            $this->runCustomersCsv();
-        } else {  //TODO this if / elseif / else would be clearer with a 'switch' statement
-            throw new Exception('Invalid database type'); // TODO what if I make a typo? should it ask again instead of quitting
         }
     }
 }
